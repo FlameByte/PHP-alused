83922e25cd6e4e8f705c7572a52255d73c6f7b7c
flamebyte <henri.lindret@khk.ee>
2018-12-13 13:12:32 +0200

    tunnitöö

---
+++ Variables/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 13.12.2018
+ * Time: 13:07
+ */
+// muutujate defineerimine
+// $muutujanimi = väärtus;
+
+$taisarv = 3;
+$reaalarv = 3.3;
+$sone = 'kolm';
+$toevaartustrue = true;
+$toevaartusfalse = false;
+// väljastame muutujad koos selgitustega
+echo 'Täisarv $taisarv';
