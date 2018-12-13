71c8e949009f981b5e3092119f6bff26f3a1ce73
flamebyte <henri.lindret@khk.ee>
2018-12-13 13:05:27 +0200

    commit test

---
+++ Print/index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+
+// tektsi väljastamine
+print('Tere Maailm!<br>');
+print('PHP Alused<br>');
+echo 'nii toimub väljastamine<br>';
+// väärtuste ühendamine.
+echo 'See ' . 'on '.'kolmest sõnast kokku pandud<br>';
+
