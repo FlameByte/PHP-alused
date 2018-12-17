53eb55900e2e0444e7418b675593a38760ab9e41
flamebyte <henri.lindret@khk.ee>
2018-12-17 14:42:56 +0200

    tunnitöö 6.3

--- functions/index.php
+++ functions/index.php
@@ -1,13 +1,16 @@
 <html>
 <head>
-    <title>Listing 6.1</title>
+    <title>Listing 6.3</title>
 </head>
 <body>
 <?php
-$num = - 321;
-$newnum = abs ( $num );
-print $newnum;
-// print "321"
+function printBR( $txt )
+{
+    print("$txt<br><\n");
+}
+printBR("this is a line");
+printBR("this is a new line");
+printBR("this is yet another line");
 ?>
 </body>
 </html>
