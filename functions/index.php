8f51ce0ee495d9066b49aaa941a455aa20a09e98
flamebyte <henri.lindret@khk.ee>
2018-12-17 15:03:21 +0200

    tunnitöö 6.10

--- functions/index.php
+++ functions/index.php
@@ -1,13 +1,12 @@
 <html>
 <head>
-    <title>Listing 6.9</title>
+    <title>Listing 6.10</title>
 </head>
 <body>
 <?php
-$num_of_calls = 0;
 function andAnotherThing( $txt )
 {
-    global $num_of_calls;
+    static $num_of_calls = 0;
     $num_of_calls++;
     print "<h1>$num_of_calls. $txt</h1>";
 }
