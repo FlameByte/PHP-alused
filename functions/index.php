bbdce41def59d5c7a64d3d2f352c971ab616592b
flamebyte <henri.lindret@khk.ee>
2018-12-17 15:07:51 +0200

    tunnitöö 6.12

--- functions/index.php
+++ functions/index.php
@@ -1,17 +1,17 @@
 <html>
 <head>
-    <title>Listing 6.11</title>
+    <title>Listing 6.12</title>
 </head>
 <body>
 <?php
-function fontWrap( $txt, $size )
+function fontWrap( $txt, $size=3 )
 {
     print "<font size=\"$size\" face=\"Helvetica,Arial,Sans-Serif\">$txt</font>";
 }
 fontWrap("A heading<br>",5);
-fontWrap("some body text<br>",3);
-fontWrap("some more body text<BR>",3);
-fontWrap("yet more body text<BR>",3);
+fontWrap("some body text<br>");
+fontWrap("some more body text<BR>");
+fontWrap("yet more body text<BR>");
 ?>
 </body>
 </html>
