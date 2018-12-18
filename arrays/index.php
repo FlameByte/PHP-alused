3f27a55ac503724adacf818de93a9532b4a931ab
flamebyte <henri.lindret@khk.ee>
2018-12-18 15:05:29 +0200

    tunnitöö

--- arrays/index.php
+++ arrays/index.php
@@ -1,6 +1,31 @@
 <?php
-$kasutajad = array(
-    'alice',
-    'bob',
-    'lucy',
-);
+// $kasutajad = array();
+// $kasutajad[] = 'alice';
+// $kasutajad[] = 'bob';
+// array_push($kasutajad,'lucy');
+
+// for($i = 0; $i < count($kasutajad); $i++) {
+//    echo $i.' - ' .$kasutajad[$i].'<br>';
+// }
+
+// foreach($kasutajad as $kasutaja){
+//    echo $kasutaja.'<br>';
+// }
+
+$kasutajad = array();
+$kasutajad[] = 'alice';
+$kasutajad[] = 'bob';
+
+array_push($kasutajad,'lucy');
+
+
+foreach($kasutajad as $kasutaja){
+    echo $kasutaja.'<br>';
+}
+
+echo '<hr>';
+array_pop($kasutajad);
+array_shift($kasutajad);
+
+foreach($kasutajad as $kasutaja){
+    echo $kasutaja.'<br>';}
