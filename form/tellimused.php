c851d7abae361b1f68dbeeb762b1ba5b531b0091
flamebyte <henri.lindret@khk.ee>
2019-01-21 12:27:33 +0200

    metshein vorm 2

--- form/tellimused.php
+++ form/tellimused.php
@@ -1,9 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: HP
- * Date: 21.01.2019
- * Time: 12:22
- */
+//lisab vormist saadud andmed muutujasse
+$toode1 = $_GET['t1'];
+$toode2 = $_GET['t2'];
+$toode3 = $_GET['t3'];
 
-<h1>Tellimine</h1>
+echo 'Toode 1: '.$toode1.'tk<br>';
+echo 'Toode 2: '.$toode2.'tk<br>';
+echo 'Toode 3: '.$toode3.'tk<br>';
+?>
