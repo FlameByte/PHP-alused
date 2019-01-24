45291f57beceb97b9bd266a9d61d306fdf40dda0
flamebyte <henri.lindret@khk.ee>
2019-01-24 13:50:12 +0200

    andmebaas - lisatud võimalus andmebaasi andmete sisestamiseks ja väljastamiseks.

--- db/index.php
+++ db/index.php
@@ -9,7 +9,17 @@ require_once 'db_fnk.php';
 require_once 'conf.php';
 
 $iktConn = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+// insert/update tüüpi sql testimine
+$sql = 'UPDATE user SET last_name = "Ebatavaline" WHERE user_id = 1';
+$res = query($sql, $iktConn);
+
+// select tüüpi sql testimine
+$sql = 'SELECT * FROM user';
+$res = getData($sql, $iktConn);
+
 echo '<pre>';
-print_r($iktConn);
+print_r($res);
+
+echo 'tere, '.$res[0]['first_name'].' '.$res[0]['last_name'].'<br>';
 
 
